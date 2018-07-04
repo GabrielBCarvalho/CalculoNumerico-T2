c9ee1ae8094e9229a75a1f8e58a6b1e6cae502c5
Gabriel Carvalho <gabrielmattheus.carvalho@gmail.com>
2018-07-04 13:41:38 -0300

    Raiz por método de Newton

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 // Calcula f(x) para um dado x
 double calculaFx(double x){
-	double res = (pow(M_E, (-pow(x, 2)) / 2)) / sqrt(2*M_PI);
+	double res = (pow(M_E, (-pow(x, 2)) / 2)) / (sqrt(2*M_PI));
 
 	return res;
 }
@@ -18,22 +18,37 @@ double simpsonComposta(double x, int N){
 	fxN = calculaFx(x);
 
 	for(int j = 1; j < N/2; j++){
-		sum1 += calculaFx(2*j*h - 1);
+		sum1 += calculaFx((2*j-1)*h);
 		sum2 += calculaFx(2*j*h);
 	}
-	sum1 += calculaFx(2*(N/2)*h);
+	sum1 += calculaFx(2*(N/2-1)*h);
 
-	integral = h/3*(fx0 + fxN + 4*sum1 + 2*sum2);
+	integral = (h/3)*(fx0 + fxN + 4*sum1 + 2*sum2);
 
 	return integral;
 }
 
-int main(int argc, char *argv[]){
-	int x, N;
-	double res;
+// Usa o método de Newton para determinar a raiz da equação descrita
+double metodoDeNewton(int N){
+	double xn1 = 0.5, xn, eps = pow(10, -10);
+	int  i =0;
+	
+	do{
+		xn = xn1;
+		xn1 = xn - ((simpsonComposta(xn, N) - 0.45) / calculaFx(xn));
+		i++;
+	}
+	while (fabs(xn1 - xn) >= eps);
+	printf("Iterações = %d\n", i);
+	return xn1;
+}
 
-	printf("Digite o valor de X a ser usado na integral: ");
-	scanf("%d", &x);
+int main(int argc, char *argv[]){
+	int N;
+	double res, x;
+
+	printf("Digite o valor de X a ser usado no intervalo da integral: ");
+	scanf("%lf", &x);
 
 	printf("Digite o valor de N a ser usado nos sub-intervalos da integral: ");
 	scanf("%d", &N);
@@ -45,7 +60,10 @@ int main(int argc, char *argv[]){
 	res = simpsonComposta(1, N) - 0.45;
 	res *= simpsonComposta(2, N) - 0.45;
 
-	printf("F(1)F(2) = %lf", res);
+	printf("F(1)F(2) = %lf\n", res);
+
+	res = metodoDeNewton(N);
+	printf("Raiz = %lf\n", res);
 
 	return 0;
 }
